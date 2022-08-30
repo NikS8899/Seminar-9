8f386961d6174600d7587c811e1d7e62b8834933
NikitaS <sergio791309@gmail.com>
2022-08-30 23:30:57 +0300

    Create Task 68 for seminar 9

---
+++ Task 64/Task 64.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string PrintNumbers (int start, int end)
+{
+// Базовый случай
+if (start == end) return start.ToString();
+// Рекурсивный случай
+return (start + ", " + PrintNumbers(start+1, end));
+}
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(M, N));
